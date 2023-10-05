8597f262b9f82a75b15b7444bc1812d2ed192682
MoacyrKennedy <kennedymoacyr@gmail.com>
2023-10-04 21:21:24 -0300

    Criando os contratos de Usuário

---
+++ src/ControleFacil.Api/Contract/Usuario/UsuarioLoginResponseContract.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControleFacil.Api.Contract.Usuario
+{
+    public class UsuarioLoginResponseContract
+    {
+        public long Id { get; set; }
+        public string email { get; set; } = string.Empty;
+        public string token { get; set; } = string.Empty;
+    }
+}
